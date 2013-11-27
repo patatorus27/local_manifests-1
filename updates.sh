9e2e58d639ae0f0fd301d27b392ca53fadef2df3
Michael Bestas <mikeioannina@gmail.com>
2013-11-27 11:10:11 +0200

    Update patches

--- updates.sh
+++ updates.sh
@@ -10,13 +10,10 @@ fi
 
 # "not enough rainbows, 1 star uninstall"
 cherries+=(36777)
-# Add support for Xperia Play touchpads
-cherries+=(45903)
-# Lower the RAM requirement for hardware acceleration
-cherries+=(51488)
+
 # libloc_api: Reduce debug level
 cherries+=(51894)
 # librpc: Reduce debug messages
 cherries+=(51896)
 
-${android}/build/tools/repopick.py -b ${cherries[@]}
+${android}/build/tools/repopick.py -b -f ${cherries[@]}
